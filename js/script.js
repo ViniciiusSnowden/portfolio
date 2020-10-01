1a55f8f2e7f99d644502f91bffa58af063201d0f
Gabriel Cruz <itsgabrielcruz@gmail.com>
2020-09-30 22:28:16 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 var hamburguer = document.querySelector(".hamburguer")
-hamburguer.addEventListener("click", function(){
-    document.querySelector(".sidebar").classList.toggle("show-menu")
+hamburguer.addEventListener("click", function() {
+    document.querySelector(".sidebar").classList.toggle("show-menu");
 });
